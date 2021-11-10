adca31bf1849d557d0edd63fddf10f32a67e3638
DownsOU <ddowns@oakland.edu>
2021-11-10 18:06:41 -0500

    changed join room functionality to be easier to call on button press

--- chozen/src/components/JoinRoom.tsx
+++ chozen/src/components/JoinRoom.tsx
@@ -1,29 +1,31 @@
 import React, {Component, useState} from 'react';
+import {IonAlert} from '@ionic/react';
+import ReactDOM from "react-dom";
 
-interface room {
-    roomData: string;
-}
 
-const JoinRoom =(props: room)=> {
-    const [confirmationMessage, setConfirmationMessage] = useState("");
-    React.useEffect(() => {
-        var request = "request_join_room "
-        request = request.concat(props.roomData);
-        const ws = new WebSocket("ws://localhost:25565");
-        ws.onopen = function() {
-            ws.send(request);
-        };
-        ws.onmessage = function(evt) {
-            if(evt.data === "request_join_room_success") {
-                setConfirmationMessage("Successfully")
-            }
+const JoinRoom = (room: string)=> {
+    var confirmationMessage: string;
+    var showConfirmation = false;
+
+    var request = "request_join_room "
+    request = request.concat(room);
+    const ws = new WebSocket("ws://192.168.1.190:25565");
+    ws.onopen = function() {
+        ws.send(request);
+    };
+    ws.onmessage = function(evt) {
+        if(evt.data === "request_join_room_success") {
+            confirmationMessage = "Successfully";
+            showConfirmation = true
+            return(
+                ReactDOM.render(
+                    <p>Joined Room {room} {confirmationMessage}</p>,
+                    document.getElementById('confirm')
+                )
+            );
         }
+    }
 
-    }, []);
-    return(
-        <div className="JoinRoom">
-            <p>Joined Room {props.roomData} {confirmationMessage}</p>
-        </div>
-    );
+
 }
 export default JoinRoom;
